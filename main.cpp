3194f56b5e8e475cc421e72c39dcd6335f217b4f
hank07820125-alt <hank07820125@gmail.com>
2026-06-20 14:50:38 +0800

    Add Skill and Item classes

--- main.cpp
+++ main.cpp
@@ -3,6 +3,79 @@
 
 using namespace std;
 
+class Item
+{
+private:
+    string name;
+    string type;
+    int effectValue;
+    int quantity;
+
+public:
+    Item(string n, string t, int e, int q)
+    {
+        name = n;
+        type = t;
+        effectValue = e;
+        quantity = q;
+    }
+
+    void use()
+    {
+        if(quantity > 0)
+        {
+            quantity--;
+            cout << name << " used!" << endl;
+        }
+    }
+
+    void showInfo()
+    {
+        cout << "Item: " << name
+             << " Quantity: "
+             << quantity << endl;
+    }
+
+    bool isAvailable()
+    {
+        return quantity > 0;
+    }
+};
+
+class Skill
+{
+private:
+    string name;
+    int damage;
+    int mpCost;
+
+public:
+    Skill(string n, int d, int mp)
+    {
+        name = n;
+        damage = d;
+        mpCost = mp;
+    }
+
+    void use()
+    {
+        cout << name << " used!" << endl;
+    }
+
+    void showInfo()
+    {
+        cout << "Skill: " << name
+             << " Damage: " << damage
+             << " MP Cost: " << mpCost
+             << endl;
+    }
+
+    int getDamage()
+    {
+        return damage;
+    }
+};
+
 class Player
 {
 private:
@@ -79,7 +152,12 @@ int main()
     Player player("Hero",100,20);
     Monster monster("Slime",50,10,20);
 
+    Skill fireball("Fireball",30,10);
+    Item potion("Potion","Heal",50,3);
+
     monster.showInfo();
+    fireball.showInfo();
+    potion.showInfo();
 
     return 0;
 }
